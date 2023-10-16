a947fe85190b843fab2fefa744b592354362a00d
Constanza Latorre <constanzalatorrecid@gmail.com>
2023-10-16 20:07:48 -0300

    lunes

--- src/lib/tinyHome.js
+++ src/lib/tinyHome.js
@@ -1,33 +1,38 @@
-import login from "./login";
-import { getPosts } from "./services";
-import { createPost } from "./services";
-import { editPost } from "./services";
-import { deletePost } from "./services";
+import { getPosts, createPost, editPost, deletePost } from "./services";
 function homeView(navigateTo) {
     const sectionPage = document.createElement('section');
     const formPost = document.createElement('form');
     const welcome = document.createElement('h3');
+    const publishButton = document.createElement('button');
+    const textArea = document.createElement('textarea');
+
     formPost.classList.add('post-form');
+    publishButton.textContent = 'Publicar';
+    publishButton.classList.add('publish-button');
+
     let quotes;
 
-    const inputThinking = document.createElement('input');
-    inputThinking.id = 'quotes';
-    inputThinking.placeholder = 'Ingresa una frase';
-    inputThinking.addEventListener("input", (e) => {
+    textArea.id = 'quotes';
+    textArea.placeholder = 'Ingresa una frase:';
+    textArea.addEventListener('input', (e) => {
         quotes = e.target.value;
+        console.log(quotes);
     });
+
+    
+
+    formPost.appendChild(textArea);
+    formPost.appendChild(publishButton);
+    sectionPage.appendChild(welcome);
+    sectionPage.appendChild(formPost);
+ 
+    return sectionPage;
+}
+
+export default homeView;
+
 /* buttonLogin.addEventListener('click', async (e) => {
     e.preventDefault()
     await register(name, email, password)
     navigateTo("/login")
 }); función ejemplo */ 
-    welcome.textContent = '¿En qué frase piensas hoy?';
-
-    formPost.appendChild(inputThinking);
-    sectionPage.appendChild(welcome);
-    sectionPage.appendChild(formPost);
-
-    return sectionPage;
-}
-
-export default homeView;
