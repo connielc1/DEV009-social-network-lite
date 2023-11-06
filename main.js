7b48383f303f4b318e8d105f0fe3416743c93830
Constanza Latorre <constanzalatorrecid@gmail.com>
2023-11-06 14:07:07 -0300

    Updates

---
+++ main.js
@@ -0,0 +1,41 @@
+import createHome from './lib/home.js';
+import login from './lib/login.js';
+import error from './lib/error.js';
+import registerView from './lib/register.js';
+import homeView from './lib/tinyHome.js';
+
+const routes = [
+  { path: '/', component: createHome },
+  { path: '/login', component: login },
+  { path: '/error', component: error },
+  { path: '/register', component: registerView },
+  { path: '/tinyHome', component: homeView },
+];
+
+const defaultRoute = '/';
+const root = document.getElementById('root');
+
+function navigateTo(hash) {
+  const route = routes.find((routeFound) => routeFound.path === hash);
+
+  if (route && route.component) {
+    window.history.pushState(
+      {},
+      route.path,
+      window.location.origin + route.path,
+    );
+
+    if (root.firstChild) {
+      root.removeChild(root.firstChild);
+    }
+    root.appendChild(route.component(navigateTo));
+  } else {
+    navigateTo('/error');
+  }
+}
+
+window.onpopstate = () => {
+  navigateTo(window.location.pathname);
+};
+
+navigateTo(window.location.pathname || defaultRoute);
